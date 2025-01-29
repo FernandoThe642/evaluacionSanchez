1c5fca6e1a2569b2a01f81401fb5a7fc1507f3b3
Fernando Sánchez Rodríguez <jsanchezr6@est.ups.edu.ec>
2025-01-29 08:45:07 -0500

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'components/presidente',
+        pathMatch: 'full' //Coincidir ruta vacia
+    },
+    {
+        path: 'components/usuario',
+        component: PresidenteComponent
+    }
+];
---
+++ src/app/components/presidente/presidente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presidente',
+  standalone: true,
+  imports: [],
+  templateUrl: './presidente.component.html',
+  styleUrl: './presidente.component.scss'
+})
+export class PresidenteComponent {
+
+}
